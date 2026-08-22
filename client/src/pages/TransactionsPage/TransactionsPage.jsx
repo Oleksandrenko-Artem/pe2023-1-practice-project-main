e17690cb42b5afe5cefa1b05ce9a3c47d67aff09
Artem Oleksandrenko <artem2008.zp@gmail.com>
2026-08-22 18:13:17 +0300

    Add transactionsSlice

--- client/src/pages/TransactionsPage/TransactionsPage.jsx
+++ client/src/pages/TransactionsPage/TransactionsPage.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/Header/Header';
+import { connect } from 'react-redux';
+import { getTransactions } from '../../store/slices/transactionsSlice';
 
-const TransactionsPage = () => {
-
-    const transactions = [
-        { id: 1, userId: 1, createdAt: "2024-01-01", operationType: "INCOME", summ: 10 },
-        { id: 2, userId: 1, createdAt: "2024-01-02", operationType: "EXPENSE", summ: 5 }
-    ]
-    const mapTransactions = t => (
-        <tr key={t.id}>
-                            <td>{t.createdAt}</td>
-                            <td>{t.operationType}</td>
-                            <td>{t.summ}</td>
-        </tr>
-    )
-    return (<>
-    <Header />
-        <main>
-            <table>
-                <caption>Your Transactions</caption>
-                <thead>
-                    <tr key={1}>Date</tr>
-                    <tr key={2}>Operation Type</tr>
-                    <tr key={3}>Summ</tr>
-                </thead>
-                <tbody>
-                    {transactions.map(mapTransactions)}
-                </tbody>
-            </table>
-        </main>
+const TransactionsPage = ({ transactions, isFetching, error, get }) => {
+  useEffect(() => {
+      get();
+  }, []);
+  const mapTransactions = (t) => (
+    <tr key={t.id}>
+      <td>{t.createdAt}</td>
+      <td>{t.operationType}</td>
+      <td>{t.summ}</td>
+    </tr>
+  );
+  return (
+    <>
+      <Header />
+      <main>
+        <table>
+          <caption>Your Transactions</caption>
+          <thead>
+            <tr key={1}>Date</tr>
+            <tr key={2}>Operation Type</tr>
+            <tr key={3}>Summ</tr>
+          </thead>
+          <tbody>{transactions.map(mapTransactions)}</tbody>
+        </table>
+      </main>
     </>
-    );
-}
+  );
+};
 
-export default TransactionsPage;
+const mapStateToProps = ({ transactionsStore }) => transactionsStore;
+
+const mapDispatchToProps = dispatch => ({
+    get: () => dispatch(getTransactions())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TransactionsPage);
---
+++ client/src/store/slices/transactionsSlice.js
@@ -0,0 +1,46 @@
+import { createAsyncChunk, createSlice } from "@reduxjs/toolkit";
+import * as restController from "./../../api/rest/restController";
+
+const TRANSACTIONS_SLICE_NAME = 'transactions';
+
+const initialState = {
+    transactions: [],
+    isFetching: false,
+    error: null,
+};
+
+export const getTransactions = createAsyncChunk(`${TRANSACTIONS_SLICE_NAME}/get`, async (payload, {rejectWithValue}) => {
+    try {
+        const { data } = await restController.getTransactions();
+        return data;
+    } catch (err) {
+        return rejectWithValue({
+            data: err?.response?.data ?? 'Gateway Timeout',
+            status: err?.response?.status ?? 504,
+        });
+    }
+});
+
+const transactionsSlice = createSlice({
+    name: TRANSACTIONS_SLICE_NAME,
+    initialState,
+    extraReducers: builder => {
+        builder.addCase(getTransactions.pending, (state) => {
+            state.isFetching = true;
+            state.error = null;
+        });
+        builder.addCase(getTransactions.fulfilled, (state, { payload }) => {
+            state.isFetching = false;
+            state.transactions = [...payload];
+            state.error = null;
+        });
+        builder.addCase(getTransactions.rejected, (state, { payload }) => {
+            state.isFetching = false;
+            state.error = payload;
+        });
+    }
+});
+
+const {reducer} = transactionsSlice;
+
+export default reducer;
